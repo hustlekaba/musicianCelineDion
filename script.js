26e697e1a1f5f87e0d1b8e17a5200edd2a63d058
hustlekaba <coummbiaa@gmail.com>
2025-04-07 20:43:22 -0700

    fisrt verison celine dion commit 04.07.2025

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+function textHtmlFunc(ids, texts, Matext)
+{
+    var vars = document.getElementById(ids);
+    vars.innerHTML = texts;
+    vars.style = Matext;
+
+}
+
+// HEAD H1 
+textHtmlFunc("h1", "Celine Dion", "color:rgb(119, 119, 74);padding-right:30px");
+// ABOUT 
+textHtmlFunc("about", "About Celine dion", "color:white;font-size:30px;font-weight:bold;padding:20px;")
+//listAlbum
+textHtmlFunc("listAlbum", "List Album", "background-color:white;padding:10px;");
