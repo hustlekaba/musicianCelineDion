4c16b05c25c5a87e40e99ea3d43c4e5f27f67f4c
hustlekaba <coummbiaa@gmail.com>
2025-04-16 16:03:26 -0700

    Modify CSS with (:hover) 4.16.2025 4:03

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 
-
 function textHtmlFunc(ids, texts, Matext)
 {
     var vars = document.getElementById(ids);
@@ -9,12 +8,22 @@ function textHtmlFunc(ids, texts, Matext)
 
 }
 
+function textshow(vars1, vars2)
+{
+    document.getElementById(vars1).innerHTML = vars2;
+}
+
 // HEAD H1 
 //textHtmlFunc("h1", "Celine Dion", "color:rgb(119, 119, 74);padding-right:30px");
 // ABOUT 
-textHtmlFunc("about", "About Celine dion", "color:white;font-size:30px;font-weight:bold;padding:20px;")
+textHtmlFunc("about", "About Celine dion", "color:white;font-size:30px;font-weight:bold;padding:20px;");
+
+//textHtmlFunc("Copyright", "cvhb", "color:white;font-size:30px;font-weight:bold;padding:20px;")
 //listAlbum
 //textHtmlFunc("listAlbum", "List Album", "background-color:white;padding:10px;");
+
+textshow("Copyrights", "Copyright © 2025 Celine Dion. All Rights Reserved.")
+textshow("network", '<li id="facebook"><a href="#">Facebook | </a></li> <li><a href="#" id="twitter">Twitter | </a></li> <li><a href="#" id="Instagram">Instagram </a></li>');
 
 // function footerBlock()
 // {
