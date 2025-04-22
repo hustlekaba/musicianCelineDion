530fe448df5d1ebd1d5e36b9fe28a1c5cabffc04
hustlekaba <coummbiaa@gmail.com>
2025-04-22 08:14:25 -0700

    modify javascript and css projet celine dion 4.22.2025

--- script.js
+++ script.js
@@ -14,6 +14,5 @@ function textshow(vars1, vars2)
 }
 
 textHtmlFunc("about", "About Celine dion", "color:white;font-size:30px;font-weight:bold;padding:20px;");
-
 textshow("Copyrights", "Copyright © 2025 Celine Dion. All Rights Reserved.")
 textshow("network", '<li id="facebook"><a href="#">Facebook | </a></li> <li><a href="#" id="twitter">Twitter | </a></li> <li><a href="#" id="Instagram">Instagram </a></li>');
